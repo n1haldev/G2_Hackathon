9621e632d9b5a11971e2dba1d6bab6ed14d50c17
Nihal T M <nihaltmdev@gmail.com>
2024-04-08 12:04:42 +0530

    Added headers to be able to scrape the data from some websites

--- scraper.py
+++ scraper.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 url = input().strip()
-response = requests.get(url)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
+response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
